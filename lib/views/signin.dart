152cd8161c6895adcae63e06c2f874dee1156ad6
abymathewtkv <abymathewtkv@gmail.com>
2021-12-09 10:46:10 +0530

    aligned children widgets at the bottom

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -14,69 +14,95 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       appBar: appBarMain(context),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: textFieldInputDecoration("username"),
-              style: simpleTextStyle(),
-            ),
-            TextField(
-              style: simpleTextStyle(),
-              decoration: textFieldInputDecoration("password"),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8.0),
-                child: Text("Forgot Password?",style: simpleTextStyle(),),
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: textFieldInputDecoration("username"),
+                style: simpleTextStyle(),
               ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              decoration:  BoxDecoration(
-                gradient: const LinearGradient(
-                colors: [
-                  Color(0xff007EF4),
-                  Color(0xff2A75BC),
-                ],
+              TextField(
+                style: simpleTextStyle(),
+                decoration: textFieldInputDecoration("password"),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+                  child: Text(
+                    "Forgot Password?",
+                    style: simpleTextStyle(),
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(30.0)
               ),
-              child: const Text("Sign in",style: TextStyle(color: Colors.white,fontSize: 17.0),) ,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              decoration:  BoxDecoration(
-                color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0)
+              const SizedBox(
+                height: 8.0,
               ),
-              child: const Text("Sign in with Google",style: TextStyle(color: Colors.black87,fontSize: 17.0),) ,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("Don't have account? ",style: TextStyle(color: Colors.white,fontSize: 17),),
-                Text("Register now",style: TextStyle(color: Colors.white,fontSize: 17),),
-              ],
-            ),
-          ],
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color(0xff007EF4),
+                        Color(0xff2A75BC),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: const Text(
+                  "Sign in",
+                  style: TextStyle(color: Colors.white, fontSize: 17.0),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: const Text(
+                  "Sign in with Google",
+                  style: TextStyle(color: Colors.black87, fontSize: 17.0),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Don't have account? ",
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  ),
+                  Text(
+                    "Register now",
+                    style: TextStyle(color: Colors.white, fontSize: 17,
+                      decoration: TextDecoration.underline,
+
+                    ),
+
+                  ),
+
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
